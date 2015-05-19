82cb36b29db3e1f010712cc3d0f1f04d60dc4744
Sean Bowen-Williams <sean.bowen-williams@recsolu.com>
2015-05-19 15:24:25 -0500

    Comment Object

--- scrape.py
+++ scrape.py
@@ -1,25 +1,37 @@
 import praw
 import pprint
 import csv
+import datetime
 
 user_agent = "Comment Karma Prediction by /u/HeteroDog"
+
+class Comment:
+	def __init__(self, body, karma, date, author):
+		self.body = body
+		self.karma = karma
+		self.date = date
+		self.author = author
+
 
 r = praw.Reddit(user_agent=user_agent)
 
 # user_name = "HeteroDog"
 # user = r.get_redditor(user_name)
-comments = {}
+comments = []
 subreddit = r.get_subreddit('askreddit')
-for submission in subreddit.get_hot(limit = 5):
+for submission in subreddit.get_hot(limit = 1):
 	forest_comments = submission.comments
 	flat_comments = praw.helpers.flatten_tree(forest_comments)
 	for comment in flat_comments:
 		try:
-			comments[comment.body] = comment.ups
+			comments.append(Comment(comment.body, 
+				comment.ups, 
+				datetime.datetime.fromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S'),
+				comment.author))
 		except AttributeError:
 			pass
 			
 # pprint.pprint(comments)
 writer = csv.writer(open('dict.csv', 'a'))
-for key, value in comments.items():
-   writer.writerow([key.encode('utf-8').strip(), value])
+for comment in comments:
+   writer.writerow([comment.body.encode('utf-8').strip(), comment.date, comment.author, comment.karma])
